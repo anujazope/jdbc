b7a3aa98a975519a18fa37c9217383f82194ba9f
anujazope <78686012+anujazope@users.noreply.github.com>
2022-02-26 10:52:02 +0530

    Add files via upload

---
+++ JDBC_ATD/src/SQL/Exercise4.java
@@ -0,0 +1,27 @@
+/*	Exercise 4 : Write a JAVA program to get  connection object using hostname , user,password 
+   stored in external properties file. */
+
+package SQL;
+
+import java.io.FileInputStream;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class Exercise4 {
+	 
+	public static void main(String[] args) throws IOException, SQLException {
+		FileInputStream fis = new FileInputStream("C:\\Users\\Dell\\Downloads\\JDBC_ATD\\src\\SQL\\config.properties");
+		Properties dbProperties = new Properties();
+		dbProperties.load(fis);
+		String HOSTNAME = dbProperties.getProperty("db.host");
+		String USER = dbProperties.getProperty("db.login");
+		String PASSWORD = dbProperties.getProperty("db.password");
+		
+		
+	 
+	}
+	}
+	
